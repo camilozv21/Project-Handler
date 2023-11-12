d5f69b8c99613b0f21c8e8451aeb588205f07045
camilozv21 <camilozv21@gmail.com>
2023-11-12 12:00:51 -0500

    Add re-resizable component to make interfaze

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,6 +1,24 @@
+import React from 'react';
+import { Resizable } from 're-resizable';
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="flex h-screen">
+      <Resizable
+        className="bg-gray-200"
+        defaultSize={{
+          width: 200,
+          height: '100%',
+        }}
+        minWidth={50}
+        maxWidth={500}
+        enable={{ top: false, right: true, bottom: false, left: false, topRight: false, bottomRight: false, bottomLeft: false, topLeft: false }}
+      >
+        <p>Sidebar</p>
+      </Resizable>
+      <div className="bg-white flex-grow">
+        <p>Contenido principal</p>
+      </div>
+    </div>
   )
 }
