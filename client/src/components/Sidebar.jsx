f28f3f4ea9958b4359487a3064b13829e36c1e84
camilozv21 <camilozv21@gmail.com>
2023-11-16 21:24:07 -0500

    Create Sidebar.jsx

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import { CreateProjectModal } from "./CreateProjectModal"
+import { useState } from "react"
+
+export const Sidebar = () => {
+  const [showCreateProject, setShowCreateProject] = useState(false)
+
+  return (
+    <>
+      <button className="p-2 border-t-blue-700" onClick={() => setShowCreateProject(true)}>Crear Proyecto +</button>
+      <hr className="mt-0 border-t-blue-900 border-t-4" />
+      <CreateProjectModal show={showCreateProject} onHide={() => setShowCreateProject(false)} />
+    </>
+  )
+}
