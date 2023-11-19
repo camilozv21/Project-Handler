1b097c13799476860117e573b6e0a76955c1f6e8
camilozv21 <camilozv21@gmail.com>
2023-11-19 16:51:11 -0500

    Update PrincipalContent.jsx

--- client/src/components/PrincipalContent.jsx
+++ client/src/components/PrincipalContent.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@apollo/client"
 import { GET_PROJECT_QUERY } from "../graphql/projectQueries"
 import { TaskCard } from "./TaskCard";
+import { TaskFormModal } from "./TaskFormModal";
+import { useEffect } from "react";
 
 export const PrincipalContent = (props) => {
   const { loading, error, data } = useQuery(GET_PROJECT_QUERY, {
@@ -8,32 +10,36 @@ export const PrincipalContent = (props) => {
       projectId: props.projectId
     }
   });
+
+  useEffect(() => {
+  }, [data])
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  console.log(data.project.project)
+
   return (
     <>
       <div className="w-full h-full">
-        <div className="flex flex-row gap-3 container py-3">
+        <div className="flex flex-row gap-3 py-3">
           <img src={data.project.project.image} alt={data.project.project.name} className="rounded w-20 h-auto" />
           <p className="text-center text-2xl font-semibold mb-0 capitalize">{data.project.project.name}</p>
         </div>
-        <div className="container border w-full h-auto p-4">
-          <div className="grid grid-cols-4 place-items-center">
+        <div className="w-full h-auto p-4">
+          <div className="grid grid-flow-row grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 place-items-center">
+            <TaskFormModal projectId={props.projectId} />
             {data.project.project.tasks && data.project.project.tasks.map((task) => {
               return (
                 <TaskCard
                   key={task.id}
                   name={task.name}
                   status={task.status}
-                  deadline={task.deadline}
+                  deadLine={task.deadLine}
                   image={task.image}
                   description={task.description}
                   projectId={props.projectId}
                 />
               )
-
             })}
           </div>
         </div>
