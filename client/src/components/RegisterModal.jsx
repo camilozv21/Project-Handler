aded72844165af201bc6ab08d5f323cfec198dbf
camilozv21 <camilozv21@gmail.com>
2023-11-19 16:51:09 -0500

    Update RegisterModal.jsx

--- client/src/components/RegisterModal.jsx
+++ client/src/components/RegisterModal.jsx
@@ -32,7 +32,6 @@ export const RegisterModal = (props) => {
           password: password1,
         },
       });
-      console.log(result.data);
       if (result.data) {
         props.onHide();
       }
@@ -111,7 +110,7 @@ export const RegisterModal = (props) => {
 
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
             >
               Registrarse
             </button>
