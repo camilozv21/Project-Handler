8942f3955292829c1000a9f1dba25c00299c38e2
camilozv21 <camilozv21@gmail.com>
2023-11-19 14:36:04 -0500

    Update ShowProjects.jsx

--- client/src/components/ShowProjects.jsx
+++ client/src/components/ShowProjects.jsx
@@ -1,18 +1,23 @@
 import { useQuery } from "@apollo/client"
 import { GET_PROJECTS_QUERY } from "../graphql/projectQueries"
 
-export const ShowProjects = () => {
+export const ShowProjects = (props) => {
   const { loading, error, data } = useQuery(GET_PROJECTS_QUERY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error}</p>;
 
+  const handleOnClick = (projectId) => {
+    props.setSelectedProjectId(projectId)
+    props.togglePrincipalContent()
+  }
+
   return (
     <>
       {data.projects.projects.map((project) => (
-        <div key={project.name} className="flex flex-row items-center gap-2 cursor-pointer">
+        <div key={project.name} className="flex flex-row items-center gap-2 cursor-pointer px-2" onClick={() => handleOnClick(project.id)}>
           <img src={project.image} alt={project.name} className="h-14 w-14 rounded-full" />
-          <p className="text-center mb-0">{project.name}</p>
+          <p className="text-center mb-0 font-semibold">{project.name}</p>
         </div>
       ))}
     </>
