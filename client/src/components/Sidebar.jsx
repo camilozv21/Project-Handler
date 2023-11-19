3237d625a0ca2d055245a87cd76dafc160b35182
camilozv21 <camilozv21@gmail.com>
2023-11-19 14:35:59 -0500

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { CreateProjectModal } from "./CreateProjectModal"
 import { useState } from "react"
 import { ShowProjects } from "./ShowProjects"
 
-export const Sidebar = () => {
+export const Sidebar = (props) => {
   const [showCreateProject, setShowCreateProject] = useState(false)
 
   return (
@@ -10,7 +10,7 @@ export const Sidebar = () => {
       <button className="p-2 border-t-blue-700" onClick={() => setShowCreateProject(true)}>Crear Proyecto +</button>
       <hr className="mt-0 border-t-blue-900 border-t-4" />
       <CreateProjectModal show={showCreateProject} onHide={() => setShowCreateProject(false)} />
-      <ShowProjects />
+      <ShowProjects togglePrincipalContent={props.togglePrincipalContent} setSelectedProjectId={props.setSelectedProjectId} />
     </>
   )
 }
