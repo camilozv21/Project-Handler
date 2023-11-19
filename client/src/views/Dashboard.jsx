3f8634e20676d0f966f9eb38d4b3954e91a82f4a
camilozv21 <camilozv21@gmail.com>
2023-11-19 14:35:50 -0500

    Update Dashboard.jsx

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Resizable } from 're-resizable';
 import { Sidebar } from '../components/Sidebar';
+import imageNoProjects from '../components/assets/noproject.png'
+import { useState } from 'react';
+import { PrincipalContent } from '../components/PrincipalContent';
 
 export const Dashboard = () => {
+  const [showPrincipalContent, setShowPrincipalContent] = useState(false)
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
+
+  const togglePrincipalContent = () => {
+    setShowPrincipalContent(!showPrincipalContent)
+  }
+
   return (
     <div className="flex h-screen">
       <Resizable
@@ -15,10 +25,14 @@ export const Dashboard = () => {
         maxWidth={300}
         enable={{ top: false, right: true, bottom: false, left: false, topRight: false, bottomRight: false, bottomLeft: false, topLeft: false }}
       >
-        <Sidebar />
+        <Sidebar togglePrincipalContent={togglePrincipalContent} setSelectedProjectId={setSelectedProjectId} />
       </Resizable>
       <div className="bg-white flex-grow">
-        <p>Contenido principal</p>
+        {showPrincipalContent ? <PrincipalContent projectId={selectedProjectId} /> :
+          <div className='flex justify-center items-center w-full h-full'>
+            <img src={imageNoProjects} alt="No project image" className='rounded shadow w-full h-auto sm:w-3/5 sm:h-3/5' />
+          </div>
+        }
       </div>
     </div>
   )
