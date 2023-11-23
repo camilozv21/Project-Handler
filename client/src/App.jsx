95c5656a78cb3ba372a528a35fdded76f58110e2
Juan Pablo Henao Rojas <juanhenao@technium.dev>
2023-11-23 13:32:37 -0500

    Add a function to check if the token is valid and use it to conditionally render the Dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "./views/Home";
 import { NavigationBar } from "./components/NavigationBar";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { Dashboard } from "./views/Dashboard";
 import NotFound from "./views/NotFound";
 
@@ -11,29 +16,52 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return {
     headers: {
       ...headers,
-      Authorization: token ? token : '',
+      Authorization: token ? token : "",
     },
   };
 });
-
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
+const isTokenValid = () => {
+  let token = localStorage.getItem("token");
+  console.log(token);
+  if (!token) {
+    return false;
+  }
+
+  let exp = localStorage.getItem("exp");
+
+  if (!exp || exp < Math.floor(Date.now() / 1000)) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("exp");
+    return false;
+  }
+
+  return true;
+};
+
 function App() {
+  const isAuthenticated = isTokenValid();
+
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {isAuthenticated ? (
+            <Route path="/dashboard" element={<Dashboard />} />
+          ) : (
+            <Route path="/dashboard" element={<Navigate to="/" />} />
+          )}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
