d5a35a7e02c0af6796924a633ea9d5af30fdae72
camilozv21 <camilozv21@gmail.com>
2023-11-25 08:51:28 -0500

    Adding spinners

--- client/src/components/CreateProjectModal.jsx
+++ client/src/components/CreateProjectModal.jsx
@@ -69,7 +69,7 @@ export const CreateProjectModal = (props) => {
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Crear
+              {loading ? <div className="spinner-border text-primary" role="status"></div> : "Crear"}
             </button>
           </form>
         </Modal.Body>
--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -70,7 +70,7 @@ export const LoginModal = (props) => {
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Iniciar sesión
+              {loading ? <div className="spinner-border text-primary" role="status"></div> : "Iniciar sesión"}
             </button>
           </form>
         </Modal.Body>
