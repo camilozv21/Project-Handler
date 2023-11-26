e9a22ff097eeb041b099ec2df2bd43e22e58be70
camilozv21 <camilozv21@gmail.com>
2023-11-26 14:41:18 -0500

    Update ShowProjects.jsx

--- client/src/components/ShowProjects.jsx
+++ client/src/components/ShowProjects.jsx
@@ -1,9 +1,12 @@
 import { useQuery } from "@apollo/client"
 import { GET_PROJECTS_QUERY } from "../graphql/projectQueries"
-import projectImg from './assets/projectdefault.png'
+import { Trash } from 'react-bootstrap-icons';
+import { DELETE_PROJECT_MUTATION } from "../graphql/projectMutations";
+import { useMutation } from "@apollo/client";
 
 export const ShowProjects = (props) => {
   const { loading, error, data } = useQuery(GET_PROJECTS_QUERY);
+  const [deleteProjectMutation, { loading: loadingDelete }] = useMutation(DELETE_PROJECT_MUTATION);
 
   if (loading) return <div className="text-center"><div
     className="spinner-border text-primary"
@@ -16,14 +19,30 @@ export const ShowProjects = (props) => {
     props.togglePrincipalContent()
   }
 
+
+  const handleDeleteProject = (projectId) => {
+    deleteProjectMutation({
+      variables: {
+        id: projectId
+      }
+    }).then(() => {
+      alert("Proyecto eliminado");
+      window.location.reload();
+    });
+  }
+
   return (
     <>
-      {data.projects.projects.map((project, i) => (
-        <div key={project.name + i} className="flex flex-row items-center gap-2 cursor-pointer px-2" onClick={() => handleOnClick(project.id)}>
-          <img src={project.imagej || projectImg} alt={project.name || 'Imagen por default'} className="h-10 w-10 rounded-full" />
-          <p className="text-center mb-0 font-semibold">{project.name}</p>
-        </div>
-      ))}
+      {data.projects.projects.map((project, i) => {
+        let imageUrl = project.image.startsWith('project') ? 'https://res.cloudinary.com/dj5kafiwa/image/upload/v1701020823/assets/project_default.jpg' : project.image;
+        return (
+          <div key={project.name + i} className="flex flex-row items-center gap-2 cursor-pointer px-2" onClick={() => handleOnClick(project.id)}>
+            <img src={imageUrl} alt={project.name} className="h-10 w-10 rounded-full" />
+            <p className="text-center mb-0 font-semibold">{project.name}</p>
+            <Trash className="text-red-500" onClick={() => handleDeleteProject(project.id)} />
+          </div>
+        )
+      })}
     </>
   );
 };
