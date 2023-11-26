ed67c28a32bdbfc16b6b0b749fa33a65461fb1eb
camilozv21 <camilozv21@gmail.com>
2023-11-26 13:09:03 -0500

    Update TaskCard.jsx

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,6 +1,37 @@
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_TASK_MUTATION } from '../graphql/taskMutations';
+
 
 export const TaskCard = (props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+
+  const handleEditTask = async (e) => {
+    try {
+      e.preventDefault();
+      const result = await addTaskMutation({
+        variables: {
+          projectId: props.projectId,
+          name: name,
+          description: description,
+          deadLine: deadeLine,
+          status: status,
+        },
+      });
+
+      if (result.data) {
+        handleClose();
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error en la mutación:", error.message);
+    }
+  }
+
   const deadlineTimestamp = Number(props.deadLine);
   if (isNaN(deadlineTimestamp)) {
     console.error(`Invalid deadline: ${props.deadLine}`);
@@ -13,14 +44,26 @@ export const TaskCard = (props) => {
   const statusColor = props.status === "Not Started" ? "red" : props.status === "In Progress" ? "yellow" : "green";
 
   return (
-    <Card className='w-28 sm:w-48 md:w-52'>
-      <Card.Body>
-        <div style={{ position: 'relative' }}>
-          <Card.Title>{props.name}</Card.Title>
-          <span style={{ backgroundColor: statusColor, borderRadius: '50%', display: 'inline-block', width: '10px', height: '10px', position: 'absolute', top: '-5px', right: '-5px' }}></span>
-        </div>
-        <Card.Text>{formattedDate}</Card.Text>
-      </Card.Body>
-    </Card>
+    <>
+      <Card className='w-28 sm:w-48 md:w-52 cursor-pointer' onClick={() => setShow(true)}>
+        <Card.Body>
+          <div style={{ position: 'relative' }}>
+            <Card.Title>{props.name}</Card.Title>
+            <span style={{ backgroundColor: statusColor, borderRadius: '50%', display: 'inline-block', width: '10px', height: '10px', position: 'absolute', top: '-5px', right: '-5px' }}></span>
+          </div>
+          <Card.Text>{formattedDate}</Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.name}</Modal.Title>
+          <span style={{ backgroundColor: statusColor, borderRadius: '50%', display: 'inline-block', width: '10px', height: '10px', marginLeft: '1rem' }}></span>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleEditTask}></form>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
