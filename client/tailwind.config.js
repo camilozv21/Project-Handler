a45770250ba29539d32ca284303f9c177855581b
camilozv21 <camilozv21@gmail.com>
2024-01-25 19:22:34 -0500

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        glow: 'glow 2s infinite',
+      },
+      keyframes: {
+        glow: {
+          '0%': { boxShadow: '0 0 3px #3b82f6, 0 0 6px #3b82f6, 0 0 9px #3b82f6, 0 0 12px #3b82f6' },
+          '50%': { boxShadow: '0 0 5px #3b82f6, 0 0 10px #3b82f6, 0 0 15px #3b82f6, 0 0 20px #3b82f6' },
+          '100%': { boxShadow: '0 0 3px #3b82f6, 0 0 6px #3b82f6, 0 0 9px #3b82f6, 0 0 12px #3b82f6' },
+        },
+      },
+    },
   },
   plugins: [],
 }
